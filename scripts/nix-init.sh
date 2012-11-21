b7b5c3feb3a4984440cc0bca9fa7e13e28150235
Mihaly Barasz <klao@google.com>
2012-11-21 23:39:21 +0100

    Fix chown to "$USER."

--- scripts/nix-init.sh
+++ scripts/nix-init.sh
@@ -7,7 +7,7 @@ export LANG=C
 
 cd /tmp
 wget http://hydra.nixos.org/build/2860047/download/1/nix-1.1-i686-linux.tar.bz2
-sudo bash -c "mkdir /nix && chown $USER /nix && chmod 0700 /nix"
+sudo bash -c "mkdir /nix && chown $USER. /nix && chmod 0700 /nix"
 ( cd / && tar xfj /tmp/nix-1.1-i686-linux.tar.bz2 /nix )
 
 # Stolen from /usr/bin/nix-finish-install & /etc/profile.d/nix.sh
