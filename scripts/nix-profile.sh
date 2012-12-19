c0492fb2e72279c97d4e17dcb87b1c4e5e68cfe3
Gergely Risko <gergely@risko.hu>
2012-12-19 21:06:10 +0100

    Added ceh_path_prepend, a proper way to add directories to PATH like variables.

--- scripts/nix-profile.sh
+++ scripts/nix-profile.sh
@@ -1,7 +1,21 @@
-export PATH=/opt/ceh/bin:$PATH
+# Prepends $1 to the front of $2 (which should be a colon separated
+# list).  If $1 is already contained in $2, deletes the old occurrence
+# first.  $2 defaults to PATH.  No-op if $1 is not a directory.
+ceh_path_prepend() {
+    new=$1
+    list=${2-PATH}
+    [ -d "$new" ] || return
+    eval "local fenced=:\$$list:"
+    local removed=${fenced/:$1:/:}
+    local trimleft=${removed#:}
+    local trimright=${trimleft%:}
+    export $list=$new:$trimright
+}
+
+ceh_path_prepend /opt/ceh/bin
 export LOCPATH=/usr/lib/locale
 export FONTCONFIG_FILE=/etc/fonts/fonts.conf
 
 # or do we want to manage our own man symlinks independently from
 # nix-env like we do for bin?
-export MANPATH=/opt/ceh/home/.nix-profile/share/man:$MANPATH
+ceh_path_prepend /opt/ceh/home/.nix-profile/share/man MANPATH
