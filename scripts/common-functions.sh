8642b96af2772dc3200db19350a8aab025ec77be
Gergely Risko <errge@google.com>
2013-01-03 16:23:22 +0100

    Added infrastructure to install nix paths on demand (ceh_nix_install and ceh_nix_exec).

--- scripts/common-functions.sh
+++ scripts/common-functions.sh
@@ -16,17 +16,43 @@ ceh_path_prepend() {
     export $list=$new:$trimright
 }
 
+# Checks if $1 is a nix path (/nix/store/whatever/*).  If not, returns.
+# If the nix path is already installed, returns.
+# Otherwise installs it with `nix-env -i'.
+ceh_nix_install() {
+  # Start a new shell with every normal output directed to stderr, so
+  # pipelines don't get confused.
+  (
+    exec >&2
+
+    local nix_postfix=${1#/nix/store/}
+    [ "$nix_postfix" = "$1" ] && return   # this is not a /nix/store path
+    local nix_dir=${nix_postfix%%/*}
+    [ -d "/nix/store/$nix_dir" ] || nix-env -i "/nix/store/$nix_dir"
+  )
+}
+
+# Used to exec a command line by first installing $1.
+ceh_nix_exec() {
+  ceh_nix_install "$1"
+  exec "$@"
+}
+
 # Executes "$@" with Nix's gcc prepended to PATH and envvars hacked to
 # include libs installed to ~/.nix-profile.
 ceh_gcc_wrapper() {
-    if [ -z "$CEH_GCC_WRAPPER_FLAGS_SET" ]; then
-	export NIX_LDFLAGS="-L /opt/ceh/home/.nix-profile/lib $NIX_LDFLAGS"
-	export NIX_CFLAGS_COMPILE="-idirafter /opt/ceh/home/.nix-profile/include $NIX_CFLAGS_COMPILE"
-	ceh_path_prepend "/opt/ceh/home/.nix-profile/lib/pkgconfig" PKG_CONFIG_PATH || true
-	ceh_path_prepend /nix/store/knyqmizvmpi8bm745zbmalksplxd10sq-gcc-wrapper-4.6.3/bin
+  if [ -z "$CEH_GCC_WRAPPER_FLAGS_SET" ]; then
+    export NIX_LDFLAGS="-L /opt/ceh/home/.nix-profile/lib $NIX_LDFLAGS"
+    export NIX_CFLAGS_COMPILE="-idirafter /opt/ceh/home/.nix-profile/include $NIX_CFLAGS_COMPILE"
+    ceh_path_prepend "/opt/ceh/home/.nix-profile/lib/pkgconfig" PKG_CONFIG_PATH || true
+    ceh_nix_install /nix/store/knyqmizvmpi8bm745zbmalksplxd10sq-gcc-wrapper-4.6.3/bin
+    ceh_path_prepend /nix/store/knyqmizvmpi8bm745zbmalksplxd10sq-gcc-wrapper-4.6.3/bin || {
+      echo >&2 "/nix/store/knyqmizvmpi8bm745zbmalksplxd10sq-gcc-wrapper-4.6.3/bin is not installed"
+      exit 1
+    }
 
-	CEH_GCC_WRAPPER_FLAGS_SET=1
-    fi
+    CEH_GCC_WRAPPER_FLAGS_SET=1
+  fi
 
-    exec "$@"
+  ceh_nix_exec "$@"
 }
