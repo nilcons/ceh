1b0169a6e9174f2c4930e1879d0275ecb01c458c
Mihaly Barasz <klao@google.com>
2013-01-09 16:28:44 +0100

    Properly set up locale where locale-archive is used

--- scripts/nix-profile.sh
+++ scripts/nix-profile.sh
@@ -1,7 +1,11 @@
 . /opt/ceh/scripts/common-functions.sh
 
 ceh_path_prepend /opt/ceh/bin
-export LOCPATH=/usr/lib/locale
+if [[ -r /usr/lib/locale/locale-archive ]]; then
+  export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+else
+  export LOCPATH=/usr/lib/locale
+fi
 export FONTCONFIG_FILE=/etc/fonts/fonts.conf
 
 # or do we want to manage our own man symlinks independently from
