8ae5006d8d431f691b6252fc936cea619ad6cc35
Gergely Risko <errge@google.com>
2013-02-19 18:21:17 +0100

    vboot_reference.nix: moved to packages/ from nix-configs/.

--- packages/vboot_reference.sh
+++ packages/vboot_reference.sh
@@ -1,6 +1,6 @@
 . /opt/ceh/scripts/common-functions.sh
 
-export NIXPKGS_CONFIG=/opt/ceh/nix-configs/vboot_reference.nix
+export NIXPKGS_CONFIG=/opt/ceh/packages/vboot_reference.nix
 ceh_nixpkgs_install vboot_reference 1.0pre23744_24b6ad4 \
     maz375zi0kh2rlai6pskbwaa707p9gb7-vboot_reference-R263701B.drv \
     2nmgvpvp1lj2j4pl6wifpiirgsknynqw-vboot_reference-R263701B
