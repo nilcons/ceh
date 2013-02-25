cb226c19952e3d83cfbed2762c5d3a3b1a9d8fb8
Gergely Risko <gergely@risko.hu>
2013-02-25 12:22:42 +0100

    Added git.

---
+++ packages/git.sh
@@ -0,0 +1,5 @@
+. /opt/ceh/scripts/common-functions.sh
+
+ceh_nixpkgs_install gitAndTools.gitFull 1.0pre24128_d8974b4 \
+    s4xzf11camk2jxmb1x0kghq9119xckb4-git-1.8.1.drv \
+    p7jj185z64siiq4lq0mynlribd7qg2ip-git-1.8.1
