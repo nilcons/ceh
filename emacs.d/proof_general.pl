02cba2e5a69ca50e48a13b21042ac2fc3452b77c
Gergely Risko <gergely@risko.hu>
2013-08-07 21:23:13 +0200

    Removed emacs.d/proof-general

--- emacs.d/proof_general.pl
+++
@@ -1,9 +0,0 @@
-#!/opt/ceh/lib/perl
-# -*- mode: perl; -*-
-
-use strict;
-use warnings;
-use lib "/opt/ceh/lib";
-use CehInstall;
-
-ceh_nixpkgs_install_for_emacs("emacs24Packages.proofgeneral", nixpkgs_version => '168115f610835654c1ed85a1bcf089f0919c9566', derivation => 'rh46vp19gchq8v9qgmmx6nnrhpamgn8p-ProofGeneral-4.2.drv', out => 'ayxydqpcpfkq641xz4r097lg9f4gmgf4-ProofGeneral-4.2');
