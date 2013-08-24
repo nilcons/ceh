659de91027195c6a4ac5852f5c35d388f290aa68
Gergely Risko <gergely@risko.hu>
2013-08-24 23:49:43 +0200

    Double duh, correct teeing

--- scripts/maintainer/install-world.sh
+++ scripts/maintainer/install-world.sh
@@ -6,7 +6,7 @@
 install () {
     echo Installing $1
     if [ "$CEH_INSTALL_WORLD_VERBOSE" != "" ]; then
-        /opt/ceh/bin/$1 --version 2>&1 | tee >$CEH_INSTALLWORLDDIR/$1.out || true
+        /opt/ceh/bin/$1 --version 2>&1 | tee $CEH_INSTALLWORLDDIR/$1.out || true
     else
         /opt/ceh/bin/$1 --version >$CEH_INSTALLWORLDDIR/$1.out 2>&1 || true
     fi
