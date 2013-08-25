7cf2d9934f705a2c28836a444e34365b42a4cc12
Gergely Risko <gergely@risko.hu>
2013-08-25 15:21:46 +0200

    coursera-dl updated

--- lib/Packages/coursera.nix
+++ lib/Packages/coursera.nix
@@ -4,25 +4,27 @@
   packageOverrides = pkgs:
     {
       coursera-dl = pkgs.callPackage (
-	{stdenv, fetchurl, makeWrapper, pythonPackages}: stdenv.mkDerivation {
-	  name = "coursera-dl-20130527";
+        {stdenv, fetchurl, makeWrapper, pythonPackages}: stdenv.mkDerivation {
+          name = "coursera-dl-20130825";
 
-	  src = fetchurl {
-	    url = "https://github.com/jplehmann/coursera/archive/300d77cf0a195cb796b982e421f780917dc89683.tar.gz";
-	    sha256 = "0d7614a85a0f3e5870cf193658e3af1024f5fbb1b52b7946c8fe7da4169ae766";
-	  };
+          src = fetchurl {
+            url = "https://github.com/jplehmann/coursera/archive/01cdfdecb008ce410195148c888439b65f1fe142.tar.gz";
+            sha256 = "0vpwwj7whpgkirx39xrwz1k15b0k17qxbngzg599xzmql9fvqi3j";
+          };
 
-	buildInputs = [ makeWrapper pythonPackages.python pythonPackages.wrapPython pythonPackages.beautifulsoup ];
+        buildInputs = [ makeWrapper pythonPackages.python pythonPackages.wrapPython pythonPackages.beautifulsoup4 pythonPackages.requests pythonPackages.six ];
 
-	installPhase = ''
+        installPhase = ''
           find
           cat requirements.txt
           ensureDir $out/bin/.wrapped
-          cp -av coursera/coursera_dl.py $out/bin/.wrapped/coursera-dl
+          cp -av coursera/* $out/bin/.wrapped
+          ( cd $out/bin/.wrapped ; find -type f -exec sed -i 's/^from \./from /' {} \; )
+          ( cd $out/bin/.wrapped ; find -type f -exec sed -i "s/ROOT, '_cache/os.getenv('HOME'), '.courseradl_cache/" {} \; )
           makeWrapper ${pythonPackages.python}/bin/python $out/bin/coursera-dl \
             --prefix PYTHONPATH : $PYTHONPATH \
-            --add-flags "$out/bin/.wrapped/coursera-dl"
+            --add-flags "$out/bin/.wrapped/coursera_dl.py"
         '';
-	}) { };
+        }) { };
     };
 }
