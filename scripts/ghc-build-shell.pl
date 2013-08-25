9f24416b7fc93028b84a5187fc669222fe5cadf2
Gergely Risko <gergely@risko.hu>
2013-08-25 16:30:01 +0200

    Try to fix autoconf vs binutils conflict

--- scripts/ghc-build-shell.pl
+++ scripts/ghc-build-shell.pl
@@ -18,11 +18,14 @@ sub ceh_nixpkgs_install_ghcbuildbin {
 ceh_nixpkgs_install_ghcbuildbin("perl", nixpkgs_version => '1f2ecd08cc28d0d199d7f0304da9d8bbc2ff6239', derivation => 'wn2dhc0mj028p7qxvhl8r98anbmgn9fm-perl-5.16.3.drv', out => 'x39yy4fg60qqgdrjhbwzrjs8r7w5wmzy-perl-5.16.3');
 ceh_nixpkgs_install_ghcbuildbin("gnumake", nixpkgs_version => '1f2ecd08cc28d0d199d7f0304da9d8bbc2ff6239', derivation => '4day37cpg5vfmyw47q1qby64c9sav32f-gnumake-3.82.drv', out => '057196vxl4ksams9fjfcgq00w9y4avdh-gnumake-3.82');
 ceh_nixpkgs_install_ghcbuildbin("m4", nixpkgs_version => '1f2ecd08cc28d0d199d7f0304da9d8bbc2ff6239', derivation => '47gc8d9vmmxx1gw2akixxw8q6abfcslg-gnum4-1.4.16.drv', out => '4p4j7jrxw5bvc74vxla5y64wcjiznz74-gnum4-1.4.16');
-ceh_nixpkgs_install_ghcbuildbin("autoconf", nixpkgs_version => '1f2ecd08cc28d0d199d7f0304da9d8bbc2ff6239', derivation => 'nvjbf6fw1q4adb6xp4fj61fwv3x4frk5-autoconf-2.69.drv', out => 'ca8ycvpcbmgjbm90ba0a5ny61kfl12d1-autoconf-2.69');
 
 # stupid info collission between autoconf and binutils
 systemdie("$CEH_NIX/bin/nix-env -p /nix/var/nix/profiles/ceh/ghc-build-bin --set-flag priority 1 autoconf >&2");
+systemdie("$CEH_NIX/bin/nix-env -p /nix/var/nix/profiles/ceh/ghc-build-bin --set-flag priority 2 binutils >&2");
+ceh_nixpkgs_install_ghcbuildbin("autoconf", nixpkgs_version => '1f2ecd08cc28d0d199d7f0304da9d8bbc2ff6239', derivation => 'nvjbf6fw1q4adb6xp4fj61fwv3x4frk5-autoconf-2.69.drv', out => 'ca8ycvpcbmgjbm90ba0a5ny61kfl12d1-autoconf-2.69');
 ceh_nixpkgs_install_ghcbuildbin("automake", nixpkgs_version => '1f2ecd08cc28d0d199d7f0304da9d8bbc2ff6239', derivation => 'gghkpks0dhsxqdyir7iqa5hkzp28ar97-automake-1.12.4.drv', out => '4bf7jxmqk5sr0i01aw3mb45ikdndr17n-automake-1.12.4');
+systemdie("$CEH_NIX/bin/nix-env -p /nix/var/nix/profiles/ceh/ghc-build-bin --set-flag priority 1 autoconf >&2");
+systemdie("$CEH_NIX/bin/nix-env -p /nix/var/nix/profiles/ceh/ghc-build-bin --set-flag priority 2 binutils >&2");
 
 # TODO(errge): get rid of this conditional
 # https://github.com/NixOS/nixpkgs/pull/857 hits the nixpkgs
