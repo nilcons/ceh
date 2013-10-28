2a77b703083b5583c2fced693b2909ca2052b2e2
Gergely Risko <gergely@risko.hu>
2013-10-28 12:09:16 +0100

    TWS bugfix

--- lib/Packages/TWS.pm
+++ lib/Packages/TWS.pm
@@ -11,7 +11,7 @@ our @EXPORT = qw($ceh_nix_install_root);
 
 $ENV{NIXPKGS_CONFIG}='/opt/ceh/lib/Packages/TWS.nix';
 if ($ENV{CEH_TWS64}) {
-    ceh_nixpkgs_install_bin64("tws", nixpkgs_version => 'e26b828f4a3f00c89497913970ca2fef8441367f', derivation => '2m8wc7phl3bw40i7p851bc08ljcazbhl-tws-20131020.drv', out => 'qmfyzr2xx8brwqsijpngixvjp5m0vc3y-tws-20131020');
+    ceh_nixpkgs_install_bin64("tws", nixpkgs_version => 'e26b828f4a3f00c89497913970ca2fef8441367f', derivation => 'b5f4ypbhbfxl6qgslgdc8r76grkx7j70-tws-20131020.drv', out => 'ckicjfxslb0q4cvawjp307br7ijhy235-tws-20131020');
 } else {
     ceh_nixpkgs_install_bin("tws", nixpkgs_version => 'e26b828f4a3f00c89497913970ca2fef8441367f', derivation => '51w88rqc2cj8qazdqgqcvwwh6pfgd91m-tws-20131020.drv', out => '00k21ilqky0xcjaa8qwipqkvhwijr2z6-tws-20131020');
 }
