ef4f99f4a6e17e2f965b9a44aaa04286d90286ac
Gergely Risko <gergely@risko.hu>
2013-10-30 22:09:50 +0100

    Revert "Upgrade nix to 1.6.1"
    This reverts commit 282224ae3351bcc32074456b42436b6b6da69cb7.
    
    Unfortunately tar --keep-old-files is silly, the upcoming
    replacment of --skip-old-files is only available from tar 1.27.
    Tar 1.27 is very new, so we have to use it from Nix, it's just
    not there on wheezy or any Ubuntu version.  It's not even there
    in Nixpkgs (pull request sent).  Once we have it in Nixpkgs,
    we have to fix ceh-upgrade.sh to use it from there and then
    retry this upgrade.
    
    :(

--- scripts/ceh-upgrade.sh
+++ scripts/ceh-upgrade.sh
@@ -70,7 +70,7 @@ echo >&2 Checks OK, upgrading to `basename $CEH_NIX`
 
 cd /tmp
 wget -c $CEH_NIX_DOWNLOAD
-( cd / && tar -x --skip-old-files -j -f /tmp/`basename $CEH_NIX_DOWNLOAD` /nix/store )
+( cd / && tar -x -k -j -f /tmp/`basename $CEH_NIX_DOWNLOAD` /nix/store )
 ( cd / && tar -x --overwrite -j -f /tmp/`basename $CEH_NIX_DOWNLOAD` /nix/store/reginfo )
 $CEH_NIX/bin/nix-store --load-db < /nix/store/reginfo
 /opt/ceh/bin/nix-env --version
