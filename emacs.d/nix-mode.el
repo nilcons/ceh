282224ae3351bcc32074456b42436b6b6da69cb7
Gergely Risko <gergely@risko.hu>
2013-10-30 21:43:34 +0100

    Upgrade nix to 1.6.1

--- emacs.d/nix-mode.el
+++ emacs.d/nix-mode.el
@@ -1,2 +1,2 @@
-(let ((nix-mode-hash "n1kz2hbi49n30bgzyxma5fyywgl6fmd1-nix-1.6"))
+(let ((nix-mode-hash "z2khn1qwap8lmxgg9iyvljcnrw6vi8zr-nix-1.6.1"))
   (load (concat "/nix/store/" nix-mode-hash "/share/emacs/site-lisp/nix-mode.el")))
--- lib/base.sh
+++ lib/base.sh
@@ -4,8 +4,8 @@
 # file is sourced from ceh-profile.sh.
 
 # TODO(errge): document that this is shared between perl and shell
-export CEH_NIX_DOWNLOAD=http://hydra.nixos.org/build/6039372/download/1/nix-1.6-i686-linux.tar.bz2
-export CEH_NIX=/nix/store/n1kz2hbi49n30bgzyxma5fyywgl6fmd1-nix-1.6
+export CEH_NIX_DOWNLOAD=http://hydra.nixos.org/build/6695693/download/1/nix-1.6.1-i686-linux.tar.bz2
+export CEH_NIX=/nix/store/z2khn1qwap8lmxgg9iyvljcnrw6vi8zr-nix-1.6.1
 
 # Prepends $1 to the front of $2 (which should be a colon separated
 # list).  If $1 is already contained in $2, deletes the old occurrence
