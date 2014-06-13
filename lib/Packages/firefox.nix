084b3895dd0117ce1eef5e049163688fa3875426
Gergely Risko <errge@nilcons.com>
2014-06-13 17:40:06 +0200

    Update firefox to 30.0

--- lib/Packages/firefox.nix
+++ lib/Packages/firefox.nix
@@ -17,9 +17,9 @@
         builder = ./firefox-builder.sh;
 
         src = fetchurl {
-          url = "https://download.mozilla.org/?product=firefox-29.0&os=linux&lang=en-US";
+          url = "https://download.mozilla.org/?product=firefox-30.0&os=linux&lang=en-US";
           name = "firefox.tar.bz2";
-          sha256 = "0vfsn7saf9as09hgbgykx9ash5vp4yxd20mr8wya7024hnvan5p6";
+          sha256 = "1djd7ayfv2p8ishf9yfy8sap2niyrlvhwrgaywb5dgwlpahl9jjb";
         };
 
         flashplayer_path = flashplayer;
