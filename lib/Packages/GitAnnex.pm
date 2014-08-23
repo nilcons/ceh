50bf68980c221c18a8db3b21181d9de30754ce2a
Gergely Risko <errge@nilcons.com>
2014-08-23 18:40:11 +0200

    Remove outdated comments

--- lib/Packages/GitAnnex.pm
+++ lib/Packages/GitAnnex.pm
@@ -9,7 +9,6 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw($ceh_nix_install_root);
 
-# TODO: https://github.com/NixOS/nixpkgs/pull/3376
 ceh_nixpkgs_install("gitAndTools.gitAnnex", nixpkgs_version => 'a38ae3c9367f9b5b2c4df437b97f3fcff294b9f7', out => 'zqgs3ld4w69smxzy7zp867ql48rnw8hx-git-annex-5.20140717');
 
 1;
