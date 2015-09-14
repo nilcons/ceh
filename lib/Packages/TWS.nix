f3f803d523ef3cf9cb562863ce6c9deb75a8cebc
Gergely Risko <errge@nilcons.com>
2015-09-14 12:07:46 +0200

    TWS update

--- lib/Packages/TWS.nix
+++ lib/Packages/TWS.nix
@@ -5,11 +5,11 @@
     {
       tws = pkgs.callPackage (
         {stdenv, fetchurl, unzip}: stdenv.mkDerivation rec {
-        name = "tws-20150908";
+        name = "tws-20150914";
 
         jts = fetchurl {
           url = "http://download2.interactivebrokers.com/java/classes/latest/jts.latest.jar";
-          sha1 = "09926331cfa185ef4fc6ed577a14623ac38b9e27";
+          sha1 = "5f95b236ca0400ca17368ea693d0dadd26f8af8b";
         };
 
         total = fetchurl {
--- lib/Packages/TWS.pm
+++ lib/Packages/TWS.pm
@@ -10,6 +10,6 @@ our @ISA = qw(Exporter);
 our @EXPORT = qw($ceh_nix_install_root);
 
 $ENV{NIXPKGS_CONFIG}='/opt/ceh/lib/Packages/TWS.nix';
-ceh_nixpkgs_install("tws", nixpkgs_version => 'e07ea5cf77601325b16f51fb457b90d5aadfab6f', out => 'kbkr7cffmz4l1lw0fnacrlagw7a1wnj6-tws-20150908');
+ceh_nixpkgs_install("tws", nixpkgs_version => 'e07ea5cf77601325b16f51fb457b90d5aadfab6f', out => '03rw3giw96nmyv898gl7qdnq0v05dih8-tws-20150914');
 
 1;
