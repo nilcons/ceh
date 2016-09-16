d96835f16b3b8c7bab4ed8481231caf6e4ad4e3d
Mihaly Barasz <klao@nilcons.com>
2016-09-16 12:08:33 +0200

    Update TWS

--- lib/Packages/TWS.pm
+++ lib/Packages/TWS.pm
@@ -10,6 +10,6 @@ our @ISA = qw(Exporter);
 our @EXPORT = qw($ceh_nix_install_root);
 
 $ENV{NIXPKGS_CONFIG}='/opt/ceh/lib/Packages/TWS.nix';
-ceh_nixpkgs_install("tws", nixpkgs_version => '551296a1cec0b9751ab96c420a7481e322ea127d', out => 'svc6fcb40x1m5qzc80p0gsy5a7agky9v-tws-20160414');
+ceh_nixpkgs_install("tws", nixpkgs_version => '551296a1cec0b9751ab96c420a7481e322ea127d', out => 'rghcmdgk0lggbl77ibfdp6jvb7aj76x5-tws-20160916');
 
 1;
